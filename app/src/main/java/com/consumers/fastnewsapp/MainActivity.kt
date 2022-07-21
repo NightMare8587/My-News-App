5593276cde78ff666de103857406491b198fd7e8
PULKIT LOYA <62491774+NightMare8587@users.noreply.github.com>
2022-07-21 13:09:59 +0530

    Update MainActivity.kt

--- app/src/main/java/com/consumers/fastnewsapp/MainActivity.kt
+++ app/src/main/java/com/consumers/fastnewsapp/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.consumers.fastnewsapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.LinearLayout
 import android.widget.ProgressBar
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
-import com.android.volley.RequestQueue
-import com.android.volley.Response
-import com.android.volley.toolbox.*
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import org.json.JSONObject
 import org.json.JSONTokener
 
@@ -20,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this,"Hello",Toast.LENGTH_LONG).show()
+//        Toast.makeText(this,"Hello",Toast.LENGTH_LONG).show()
         val authorList = ArrayList<String>()
         val titleList = ArrayList<String>()
         val descriptionList = ArrayList<String>()
